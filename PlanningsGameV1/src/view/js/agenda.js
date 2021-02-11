df9df036058966221d5f0482d67fe91a45a5d434
Raf Vanherbergen <>
2021-02-11 11:11:31 +0100

    launch.json added to enable debug

--- PlanningsGameV1/src/view/js/agenda.js
+++ PlanningsGameV1/src/view/js/agenda.js
@@ -1,13 +1,12 @@
-const renderAgenda = () => {
+function renderAgenda() {
 
 let days = "";
 
 const htmlDays = daydocument.querySelector(".days");
 
-for (let i=1; i<=42; i++){
+for (let i=1; i<=42; i++)
+{
   days += `<div>${i}</div>`;
-  
-
 }
 
 htmlDays.innerHTML = days;
