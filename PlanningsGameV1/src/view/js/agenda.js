62a2a6c4e55aac62c0b7ac11cfa0e73610189feb
Raf Vanherbergen <>
2021-02-12 14:05:48 +0100

    PlanningsGame V1.0.9: possible to select/deselect day + permanent select

--- PlanningsGameV1/src/view/js/agenda.js
+++ PlanningsGameV1/src/view/js/agenda.js
@@ -1,15 +1,74 @@
-function renderAgenda() {
-
 let days = "";
 
-const htmlDays = daydocument.querySelector(".days");
+let selectedDaysArray = [[]];
+let alreadySelectedId = -1;
+let currentPatient = "koekje";
+for (let i=0; i<=41; i++)
+{ 
+  if(i != 5)
+  {
+    selectedDaysArray[0].push(false);
+  }
+  else
+  {
+    selectedDaysArray[0].push(currentPatient);
+  }
+} 
+//console.log(selectedDaysArray[0]);
 
-for (let i=1; i<=42; i++)
+const htmlDays = document.querySelector(".days");
+
+function renderAgenda() 
 {
-  days += `<div>${i}</div>`;
-}
-
-htmlDays.innerHTML = days;
+  alreadySelectedId = -1;
+  for (let i=0; i<=41; i++)
+  {
+    if( selectedDaysArray[0][i] == false)
+    {
+    //days += `<div id=day${i} onClick="selectDay(this.id)">${i}</div>`;
+    days += `<div class=day id=d${i}>${i}</div>`;
+    }
+    else
+    {
+      days += `<div class="day, selectedDay" id=d${i}>${i}</div>`;
+    }  
+  }
+  htmlDays.innerHTML = days;
 }
 
 renderAgenda();
+
+document.querySelectorAll(".day").forEach
+(day => {
+  day.addEventListener("click", event => {
+    
+    if(event.currentTarget.classList.value == "day")
+    {
+      if(alreadySelectedId!=-1)
+      {
+        document.getElementById(alreadySelectedId).classList.remove("selectedDay");
+      }
+
+      alreadySelectedId = event.currentTarget.id;
+      event.currentTarget.classList.add("selectedDay");
+    }
+    else
+    {
+      if(alreadySelectedId != -1)
+      {
+        event.currentTarget.classList.remove("selectedDay");
+      }     
+    }            
+  });
+});
+
+function addSelectedDay()
+{
+  document.getElementById(alreadySelectedId).innerHTML= currentPatient;
+  let dayNr = alreadySelectedId.charAt(1)
+  selectedDaysArray[0][dayNr] = currentPatient;
+
+  alreadySelectedId = -1;
+
+}
+
