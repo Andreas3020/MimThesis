8e6f3f76704598d98e4b8b32b41038d5cb395bd9
Raf Vanherbergen <>
2021-02-16 13:40:48 +0100

    PlanningsGame V1.1.4: agenda is able to go to next weeks and remember prev

--- PlanningsGameV1/src/view/js/agenda.js
+++ PlanningsGameV1/src/view/js/agenda.js
@@ -1,74 +1,65 @@
 //Init variables
-let days = "";
 
-let selectedDaysArray = [[]];
+
+let selectedDaysArray = [];
 let alreadySelectedId = -1;
 let currentPatient = "koekje";
-
+let days = ""
 const htmlDays = document.querySelector(".days");
+let addedWeeksToArrayNr = 0;
+let week = 0;
 
 //tableBody = document.getElementById('patientTable');
 //let currentPatient = tableBody.rows[1].cells[1].innerHTML;
 
 //Create initial DataArray
-for (let i=0; i<=41; i++)
-{ 
-  if(i != 5)
-  {
-    selectedDaysArray[0].push(false);
-  }
-  else
-  {
-    selectedDaysArray[0].push(currentPatient);
-  }
-} 
-//console.log(selectedDaysArray[0]);
+addWeekToArray();
 
+// Adds week to selectdDaysArray
+function addWeekToArray()
+{
+  selectedDaysArray.push([]);
+  for (let i=0; i<=41; i++)
+  { 
+    
+    selectedDaysArray[week].push(false);
+    
+    //selectedDaysArray[0].push(currentPatient);    
+  } 
+}
 
 //Display Agenda
 function renderAgenda() 
 {
+  console.log(week);
+  days = "";
+  
+
   alreadySelectedId = -1;
   for (let i=0; i<=41; i++)
   {
-    if( selectedDaysArray[0][i] == false)
+    if( selectedDaysArray[week][i] == false)
     {
     //days += `<div id=day${i} onClick="selectDay(this.id)">${i}</div>`;
     days += `<div class=day id=d${i}>${i}</div>`;
     }
     else
     {
-      days += `<div class="day, selectedDay" id=d${i}>${i}</div>`;
+      days += `<div class="day, selectedDay" id=d${i}>${selectedDaysArray[week][i]}</div>`;
     }  
   }
+  console.log(days);
   htmlDays.innerHTML = days;
+
+  addEventlistenerToDays()
+
+  document.getElementById('h2Weeknr').innerHTML= week + 1 ;
+  
 }
 
 renderAgenda();
 
-document.querySelectorAll(".day").forEach
-(day => {
-  day.addEventListener("click", event => {
-    
-    if(event.currentTarget.classList.value == "day")
-    {
-      if(alreadySelectedId!=-1)
-      {
-        document.getElementById(alreadySelectedId).classList.remove("selectedDay");
-      }
 
-      alreadySelectedId = event.currentTarget.id;
-      event.currentTarget.classList.add("selectedDay");
-    }
-    else
-    {
-      if(alreadySelectedId != -1)
-      {
-        event.currentTarget.classList.remove("selectedDay");
-      }     
-    }            
-  });
-});
 
 function addSelectedDay()
 {
@@ -76,10 +67,59 @@ function addSelectedDay()
   currentPatient = tableBody.rows[1].cells[1].innerHTML;
 
   document.getElementById(alreadySelectedId).innerHTML= currentPatient;
-  let dayNr = alreadySelectedId.charAt(1)
-  selectedDaysArray[0][dayNr] = currentPatient;
+  let dayNr = alreadySelectedId.match(/\d+/);
+  
+  selectedDaysArray[week][dayNr] = currentPatient;
+
 
   alreadySelectedId = -1;
 
 }
 
+document.querySelector(".prev").addEventListener("click", () => {
+  if(week > 0)
+  {
+    week -= 1;
+  renderAgenda();
+  }
+  
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  week += 1;
+  if(week>addedWeeksToArrayNr)
+  { 
+    addedWeeksToArrayNr += 1;
+    addWeekToArray();
+  }
+  renderAgenda();
+  console.log(selectedDaysArray);
+});
+
+function addEventlistenerToDays()
+{
+  document.querySelectorAll(".day").forEach
+  (day => {
+    day.addEventListener("click", event => {
+      
+      if(event.currentTarget.classList.value == "day")
+      {
+        if(alreadySelectedId!=-1)
+        {
+          document.getElementById(alreadySelectedId).classList.remove("selectedDay");
+        }
+
+        alreadySelectedId = event.currentTarget.id;
+        event.currentTarget.classList.add("selectedDay");
+      }
+      else
+      {
+
+        if(alreadySelectedId != -1)
+        {
+          event.currentTarget.classList.remove("selectedDay");
+        }     
+      }            
+    });
+  });
+}
