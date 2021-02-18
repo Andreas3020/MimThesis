9747a7533e78f6cc2abfaeaf70683a9e5f049cce
Raf Vanherbergen <>
2021-02-18 12:42:06 +0100

    PlanningsGame V1.2.0: Agenda revamped

--- PlanningsGameV1/src/view/js/agenda.js
+++ PlanningsGameV1/src/view/js/agenda.js
@@ -1,129 +1,98 @@
-//Init variables
+/* i=days, j=hourslots and k=oncoChemoslot -> 6*i + 42*j+k = slotnr in array */
 
+//init variables
 
-let selectedDaysArray = [];
+let selectedSlotsArray = [];
 let alreadySelectedId = -1;
-let currentPatient = "koekje";
-let days = ""
-const htmlDays = document.querySelector(".days");
+let currentPatient = "";
+
+let weekdaysShort = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+let weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
 let addedWeeksToArrayNr = 0;
 let week = 0;
-
-//tableBody = document.getElementById('patientTable');
-//let currentPatient = tableBody.rows[1].cells[1].innerHTML;
 
 //Create initial DataArray
 addWeekToArray();
 
+renderAgenda();
+
 // Adds week to selectdDaysArray
 function addWeekToArray()
 {
-  selectedDaysArray.push([]);
-  for (let i=0; i<=41; i++)
-  { 
-    
-    selectedDaysArray[week].push(false);
-    
-    //selectedDaysArray[0].push(currentPatient);    
+  selectedSlotsArray.push([]);
+  for (let i=0; i<=839; i++)
+  {    
+    selectedSlotsArray[week].push(false);        
   } 
 }
 
 //Display Agenda
 function renderAgenda() 
 {
-  console.log(week);
-  days = "";
   
-
+  let slots = ""
+  const htmlSlots = document.querySelector(".slots"); 
   alreadySelectedId = -1;
-  for (let i=0; i<=41; i++)
+
+  //for every hourSlot 
+  for(let j=0; j<20; j++) //verticaal
   {
-    if( selectedDaysArray[week][i] == false)
+    //for every day
+    for (let i=0; i<7; i++) //horizontaal
     {
-    //days += `<div id=day${i} onClick="selectDay(this.id)">${i}</div>`;
-    days += `<div class=day id=d${i}>${i}</div>`;
+      let weekday = weekdaysShort[i];
+      // for 2 onco and 4 chemo
+      for (let k=0; k<6; k++) //horizontaal
+      { 
+        if(selectedSlotsArray[week][6*i + 42*j+k] == false)
+        {                  
+          slots += `<div class="${weekday}, slot" id=D${i}_H${j}_OC${k}>${6*i + 42*j+k}</div>`;          
+        }
+        else
+        {                    
+          slots += `<div class="${weekday}, slot, selectedSlot" id=D${i}_H${j}_OC${k}>${selectedSlotsArray[week][6*i + 42*j+k].charAt(0)}</div>`;          
+        }
+      }      
     }
-    else
-    {
-      days += `<div class="day, selectedDay" id=d${i}>${selectedDaysArray[week][i]}</div>`;
-    }  
-  }
-  console.log(days);
-  htmlDays.innerHTML = days;
+  }      
+  htmlSlots.innerHTML = slots;
 
-  addEventlistenerToDays()
-
-  document.getElementById('h2Weeknr').innerHTML= week + 1 ;
+  let weekNr= week +1;
   
+  document.getElementById('weekNr').innerHTML= "week " + weekNr  ;
+
+  addEventlistenerToSlots()
 }
 
-renderAgenda();
-
-
-
-function addSelectedDay()
+function addSelectedSlot()
 {
   tableBody = document.getElementById('patientTable');
   currentPatient = tableBody.rows[1].cells[1].innerHTML;
   let avDay = tableBody.rows[1].cells[3].innerHTML;
   
   try{
-    let dayNr = parseInt(document.getElementById(alreadySelectedId).innerHTML,10);
-    if(avDay == "Monday"){
-      if([0,7,14,21,28,35].includes(dayNr)){
-        add(currentPatient, dayNr);
-      }
-      else{
-        window.alert('The patient has not been alloted a correct timeslot!');
-      }
+   
+   //get the id from your agenda
+   let a = alreadySelectedId.match(/\d+/).input;
+    
+   //checking the day number and the slot number and type
+   /*let idArray = a.split("_")
+   let strArray = idArray[0].split("d");*/
+   let strArray = a.split("_");
+   let dayStr = strArray[0];
+   let hourSlotStr = strArray[1];
+   let oncoChemoStr = strArray[2];
+   let dayNr = parseInt(dayStr.split("D")[1],10);
+   let hourSlot = parseInt(hourSlotStr.split("H")[1],10);
+   let oncoChemoNr = parseInt(oncoChemoStr.split("OC")[1],10);
+   let slotNr = 6*dayNr + 42*hourSlot  + oncoChemoNr;
+   
+   if(weekdays[dayNr] == avDay){
+      add(currentPatient, slotNr);
     }
-    else if(avDay == "Tuesday"){
-      if([1,8,15,22,29,36].includes(dayNr)){
-        add(currentPatient, dayNr);
-      }
-      else{
-        window.alert('The patient has not been alloted a correct timeslot!');
-      }
-    }
-    else if(avDay == "Wednesday"){
-      if([2,9,16,23,30,37].includes(dayNr)){
-        add(currentPatient, dayNr);
-      }
-      else{
-        window.alert('The patient has not been alloted a correct timeslot!');
-      }
-    }
-    else if(avDay == "Thursday"){
-      if([3,10,17,24,31,38].includes(dayNr)){
-        add(currentPatient, dayNr);
-      }
-      else{
-        window.alert('The patient has not been alloted a correct timeslot!');
-      }
-    }
-    else if(avDay == "Friday"){
-      if([4,11,18,25,32,39].includes(dayNr)){
-        add(currentPatient, dayNr);
-      }
-      else{
-        window.alert('The patient has not been alloted a correct timeslot!');
-      }
-    }
-    else if(avDay == "Saturday"){
-      if([5,12,19,26,33,40].includes(dayNr)){
-        add(currentPatient, dayNr);
-      }
-      else{
-        window.alert('The patient has not been alloted a correct timeslot!');
-      }
-    }
-    else if(avDay == "Sunday"){
-      if([6,13,20,27,34,41].includes(dayNr)){
-        add(currentPatient, dayNr);
-      }
-      else{
-        window.alert('The patient has not been alloted a correct timeslot or no timeslot has been selected!\nClick on skip if you are not able to allot a correct timeslot to the patient.');
-      }
+   else{
+      window.alert('The patient has not been alloted a correct timeslot!');
     }
   }
   catch{
@@ -131,13 +100,40 @@ function addSelectedDay()
   }
 }
 
-function add(currentPatient, dayNr){
+function add(currentPatient, slotNr){
   let a = nextPatientEvent();
   if(a <= 1){
-    document.getElementById(alreadySelectedId).innerHTML= currentPatient;
-    selectedDaysArray[week][dayNr] = currentPatient;
+    document.getElementById(alreadySelectedId).innerHTML= currentPatient.charAt(0);
+    selectedSlotsArray[week][slotNr] = currentPatient;
     alreadySelectedId = -1;
   }
+}
+
+function addEventlistenerToSlots()
+{
+  document.querySelectorAll(".slot").forEach
+  (slot => {
+    slot.addEventListener("click", event => {
+      if(event.currentTarget.classList.value.split(", ")[1]== "slot")
+      {
+        if(alreadySelectedId!=-1)
+        {
+          document.getElementById(alreadySelectedId).classList.remove("selectedSlot");
+        }
+
+        alreadySelectedId = event.currentTarget.id;
+        event.currentTarget.classList.add("selectedSlot");
+      }
+      else
+      {
+
+        if(alreadySelectedId != -1)
+        {
+          event.currentTarget.classList.remove("selectedSlot");
+        }     
+      }            
+    });
+  });
 }
 
 document.querySelector(".prev").addEventListener("click", () => {
@@ -157,33 +153,7 @@ document.querySelector(".next").addEventListener("click", () => {
     addWeekToArray();
   }
   renderAgenda();
-  console.log(selectedDaysArray);
+  
 });
 
-function addEventlistenerToDays()
-{
-  document.querySelectorAll(".day").forEach
-  (day => {
-    day.addEventListener("click", event => {
-      
-      if(event.currentTarget.classList.value == "day")
-      {
-        if(alreadySelectedId!=-1)
-        {
-          document.getElementById(alreadySelectedId).classList.remove("selectedDay");
-        }
-
-        alreadySelectedId = event.currentTarget.id;
-        event.currentTarget.classList.add("selectedDay");
-      }
-      else
-      {
-
-        if(alreadySelectedId != -1)
-        {
-          event.currentTarget.classList.remove("selectedDay");
-        }     
-      }            
-    });
-  });
-}
+  
