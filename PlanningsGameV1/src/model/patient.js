3769bc0aee51918a9688c2e7e0f6f96b79628a42
Andreas Dierickx <andreasdierickx@hotmail.com>
2021-04-20 16:33:47 +0200

    planningsgame: V1.5.12: moeilijkeheden trial 1

--- PlanningsGameV1/src/model/patient.js
+++ PlanningsGameV1/src/model/patient.js
@@ -23,6 +23,7 @@ var maxChemoLength;
 var nrOfAvailableDays;
 var availableOneProb;
 var availableTwoProb;
+var nrPatientsCurrentDay;
 
 Patient.genList = {};
 Patient.list = {};
@@ -43,8 +44,9 @@ Patient.Easy = function(){
   maxLength = 8;
   oncoProb = 0.7;
   nrOfPersons = 50;
-  minPatDay = 2; //3
+  minPatDay = 3; //3
   maxPatDay = 4; //5
+  nrPatientsCurrentDay = getRandomInt(minPatDay,maxPatDay);
   maxChemoLength = 3;
   nrOfAvailableDays;// = 3;
   availableOneProb = 0.2;
@@ -55,32 +57,31 @@ Patient.Moderate = function(){
   minLength = 5;
   maxLength = 8;
   oncoProb = 0.7;
-  nrOfPersons = 50;
-  minPatDay = 2; //3
-  maxPatDay = 4; //5
-  maxChemoLength = 3;
+  nrOfPersons = 60;
+  minPatDay = 3; //3
+  maxPatDay = 5; //5
+  nrPatientsCurrentDay = getRandomInt(minPatDay,maxPatDay);
+  maxChemoLength = 4;
   nrOfAvailableDays;// = 3;
-  availableOneProb = 0.2;
-  availableTwoProb = 0.6;
+  availableOneProb = 0.3;
+  availableTwoProb = 0.7;
   // hier bloedtest nog toevoegen
 }
 
 Patient.Hard = function(){
   minLength = 5;
   maxLength = 8;
-  oncoProb = 0.8;
-  nrOfPersons = 50;
-  minPatDay = 3; //3
-  maxPatDay = 4; //5
-  maxChemoLength = 3;
+  oncoProb = 0.7;
+  nrOfPersons = 70;
+  minPatDay = 4; //3
+  maxPatDay = 6; //5
+  nrPatientsCurrentDay = getRandomInt(minPatDay,maxPatDay);
+  maxChemoLength = 4;
   nrOfAvailableDays;// = 3;
-  availableOneProb = 0.3;
+  availableOneProb = 0.4;
   availableTwoProb = 0.8;
   // hier bloedtest vaker laten falen
 }
-
-//AMOUNT PATIENTS TO BE ASSIGNED ON CURRENT DAY (updated per day.)
-var nrPatientsCurrentDay = getRandomInt(minPatDay,maxPatDay);
 
 // PATIENT LIST LOCALSTORAGE (Generate + save)
 Patient.generate = function() {
